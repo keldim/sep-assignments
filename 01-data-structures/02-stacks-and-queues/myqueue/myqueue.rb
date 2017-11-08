3b6fe96786fbfaf930f246b1f23188075cc32afc
Chris Yoo <woyoo@umich.edu>
2017-11-08 16:10:27 +0900

    stacks and queues

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -1,18 +1,30 @@
 class MyQueue
   attr_accessor :head
   attr_accessor :tail
+  attr_accessor :lastIndex
 
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @lastIndex = 0
   end
 
   def enqueue(element)
+    @queue[@lastIndex] = element
+    @head = @queue[0]
+    @tail = @queue[@lastIndex]
+    self.lastIndex += 1
   end
 
   def dequeue
+    @queue.delete_at(0)
   end
 
   def empty?
+    if @queue.length > 0
+      return false
+    else
+      return true
+    end
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -1,17 +1,40 @@
 class MyStack
   attr_accessor :top
+  attr_accessor :lastIndex
 
   def initialize
     @stack = Array.new
     self.top = nil
+    self.lastIndex = nil
   end
 
   def push(item)
+    if self.lastIndex == nil
+      self.lastIndex = 0
+    else
+      self.lastIndex += 1
+    end
+    @stack[self.lastIndex] = item
+    self.top = item
   end
 
   def pop
+    temp = @stack[self.lastIndex]
+    if self.lastIndex == 0
+      self.lastIndex = nil
+      self.top = nil
+    else
+      self.lastIndex -= 1
+      self.top = @stack[self.lastIndex]
+    end
+    return temp
   end
 
   def empty?
+    if @stack.length > 0
+      return false
+    else
+      return true
+    end
   end
 end
