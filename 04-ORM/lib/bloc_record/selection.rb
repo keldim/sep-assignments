dc1ae2f59c4d19307788317e22029143006d2f4d
Chris Yoo <woyoo@umich.edu>
2018-01-25 02:55:41 -0500

    Databases: Where, Order, and Joins checkpoint

--- 04-ORM/lib/bloc_record/selection.rb
+++ 04-ORM/lib/bloc_record/selection.rb
@@ -26,66 +26,132 @@ module Selection
   end
 
   def find_by(attribute, value)
-       row = connection.get_first_row <<-SQL
-         SELECT #{columns.join ","} FROM #{table}
-         WHERE #{attribute} = #{BlocRecord::Utility.sql_strings(value)};
-       SQL
+    row = connection.get_first_row <<-SQL
+    SELECT #{columns.join ","} FROM #{table}
+    WHERE #{attribute} = #{BlocRecord::Utility.sql_strings(value)};
+    SQL
 
-       init_object_from_row(row)
+    init_object_from_row(row)
+  end
+
+
+  def take(num=1)
+    if num > 1
+      rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      ORDER BY random()
+      LIMIT #{num};
+      SQL
+
+      rows_to_array(rows)
+    else
+      take_one
+    end
+  end
+
+
+  def take_one
+    row = connection.get_first_row <<-SQL
+    SELECT #{columns.join ","} FROM #{table}
+    ORDER BY random()
+    LIMIT 1;
+    SQL
+
+    init_object_from_row(row)
+  end
+
+  def first
+    row = connection.get_first_row <<-SQL
+    SELECT #{columns.join ","} FROM #{table}
+    ORDER BY id ASC LIMIT 1;
+    SQL
+
+    init_object_from_row(row)
+  end
+
+  def last
+    row = connection.get_first_row <<-SQL
+    SELECT #{columns.join ","} FROM #{table}
+    ORDER BY id DESC LIMIT 1;
+    SQL
+
+    init_object_from_row(row)
+  end
+
+
+  def all
+    rows = connection.execute <<-SQL
+    SELECT #{columns.join ","} FROM #{table};
+    SQL
+
+    rows_to_array(rows)
+  end
+
+  def where(*args)
+    if args.count > 1
+      expression = args.shift
+      params = args
+
+    else
+      case args.first
+      when String
+        expression = args.first
+      when Hash
+        expression_hash = BlocRecord::Utility.convert_keys(args.first)
+        expression = expression_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+
+      end
+    end
+
+
+
+    sql = <<-SQL
+    SELECT #{columns.join ","} FROM #{table}
+    WHERE #{expression};
+    SQL
+
+    rows = connection.execute(sql, params)
+    rows_to_array(rows)
+  end
+
+  def order(*args)
+    if args.count > 1
+      order = args.join(",")
+    else
+      order = args.first.to_s
+    end
+
+
+    rows = connection.execute <<-SQL
+    SELECT * FROM #{table}
+    ORDER BY #{order};
+    SQL
+    rows_to_array(rows)
+  end
+
+
+  def join(*args)
+     if args.count > 1
+       joins = args.map { |arg| "INNER JOIN #{arg} ON #{arg}.#{table}_id = #{table}.id"}.join(" ")
+       rows = connection.execute <<-SQL
+         SELECT * FROM #{table} #{joins}
+       SQL
+     else
+       case args.first
+       when String
+         rows = connection.execute <<-SQL
+           SELECT * FROM #{table} #{BlocRecord::Utility.sql_strings(args.first)};
+         SQL
+       when Symbol
+         rows = connection.execute <<-SQL
+           SELECT * FROM #{table}
+           INNER JOIN #{args.first} ON #{args.first}.#{table}_id = #{table}.id
+         SQL
+       end
      end
 
-
-     def take(num=1)
-          if num > 1
-            rows = connection.execute <<-SQL
-              SELECT #{columns.join ","} FROM #{table}
-              ORDER BY random()
-              LIMIT #{num};
-            SQL
-
-            rows_to_array(rows)
-          else
-            take_one
-          end
-        end
-
-
-     def take_one
-          row = connection.get_first_row <<-SQL
-            SELECT #{columns.join ","} FROM #{table}
-            ORDER BY random()
-            LIMIT 1;
-          SQL
-
-          init_object_from_row(row)
-        end
-
-        def first
-            row = connection.get_first_row <<-SQL
-              SELECT #{columns.join ","} FROM #{table}
-              ORDER BY id ASC LIMIT 1;
-            SQL
-
-            init_object_from_row(row)
-          end
-
-          def last
-            row = connection.get_first_row <<-SQL
-              SELECT #{columns.join ","} FROM #{table}
-              ORDER BY id DESC LIMIT 1;
-            SQL
-
-            init_object_from_row(row)
-          end
-
-
-          def all
-              rows = connection.execute <<-SQL
-                SELECT #{columns.join ","} FROM #{table};
-              SQL
-
-              rows_to_array(rows)
-            end
+     rows_to_array(rows)
+   end
 
 
   private
