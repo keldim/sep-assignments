5736640e8824a0c759034506c9d7acdc28d0674f
Chris Yoo <woyoo@umich.edu>
2018-01-25 09:03:45 -0500

    Databases: Update checkpoint

--- 04-ORM/lib/bloc_record/persistence.rb
+++ 04-ORM/lib/bloc_record/persistence.rb
@@ -32,7 +32,22 @@ module Persistence
     true
   end
 
+
+  def update_attribute(attribute, value)
+     self.class.update(self.id, { attribute => value })
+   end
+
+   def update_attributes(updates)
+        self.class.update(self.id, updates)
+      end
+
+
   module ClassMethods
+
+    def update_all(updates)
+          update(nil, updates)
+        end
+
     def create(attrs)
       attrs = BlocRecord::Utility.convert_keys(attrs)
       attrs.delete "id"
@@ -47,6 +62,32 @@ module Persistence
       data["id"] = connection.execute("SELECT last_insert_rowid();")[0][0]
       new(data)
     end
+
+  def update(ids, updates)
+           # #1
+           updates = BlocRecord::Utility.convert_keys(updates)
+           updates.delete "id"
+           # #2
+           updates_array = updates.map { |key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}" }
+
+           if ids.class == Fixnum
+          where_clause = "WHERE id = #{ids};"
+        elsif ids.class == Array
+          where_clause = ids.empty? ? ";" : "WHERE id IN (#{ids.join(",")});"
+        else
+          where_clause = ";"
+        end
+
+           # #3
+           connection.execute <<-SQL
+             UPDATE #{table}
+             SET #{updates_array * ","} #{where_clause}
+           SQL
+
+           true
+         end
+
+
   end
 
 
